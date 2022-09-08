7586898e48673717a532add80be489f4b14493e7
MuhammaddiyorRR <muhammaddiyorrr@gmail.com>
2022-09-08 09:38:30 +0500

    m

---
+++ src/App.js
@@ -0,0 +1,93 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="nav">
+        <h2>Олег Марков</h2>
+        <ul>
+          <li>
+            <a
+              href="/Главная
+"
+            >
+              Главная
+            </a>
+          </li>
+          <li>
+            <a
+              href=" /
+Обо мне
+          "
+            >
+              Обо мне
+            </a>
+          </li>
+          <li>
+            <a
+              href="/
+Связаться
+          "
+            >
+              Связаться
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="container">
+        <div className="image">
+          <img
+            src="https://static.wixstatic.com/media/d78df6_4517dc1e0b1545bf84dccd915203c870.jpg/v1/fill/w_378,h_378,al_c,q_80,usm_0.66_1.00_0.01/d78df6_4517dc1e0b1545bf84dccd915203c870.jpg"
+            alt=""
+          />
+          <img
+            src="https://static.wixstatic.com/media/d78df6_3eea4c192eed4667a602ead5652459c5.jpg/v1/fill/w_378,h_378,al_c,q_80,usm_0.66_1.00_0.01/d78df6_3eea4c192eed4667a602ead5652459c5.jpg"
+            alt=""
+          />
+          <img
+            src="https://static.wixstatic.com/media/d78df6_b9747effc3074458a295cf5674f8c73c.jpg/v1/fill/w_378,h_378,al_c,q_80,usm_0.66_1.00_0.01/d78df6_b9747effc3074458a295cf5674f8c73c.jpg"
+            alt=""
+          />
+        </div>
+        <div className="image">
+          <img
+            src="https://static.wixstatic.com/media/d78df6_9f4f79dd27814710b261e3acb931156e.jpg/v1/fill/w_378,h_566,al_c,q_80,usm_0.66_1.00_0.01/d78df6_9f4f79dd27814710b261e3acb931156e.jpg"
+            alt=""
+          />
+          <img
+            src="https://static.wixstatic.com/media/d78df6_b572be78b8984e2db58978eb21e433d7.jpg/v1/fill/w_378,h_378,al_c,q_80,usm_0.66_1.00_0.01/d78df6_b572be78b8984e2db58978eb21e433d7.jpg"
+            alt=""
+          />
+          <img
+            src="https://static.wixstatic.com/media/d78df6_394a136048754355ad8432b30522c761.jpg/v1/fill/w_378,h_378,al_c,q_80,usm_0.66_1.00_0.01/d78df6_394a136048754355ad8432b30522c761.jpg"
+            alt=""
+          />
+        </div>
+        <div className="image">
+          <img
+            src="https://static.wixstatic.com/media/d78df6_914d12d64a57420c99737b9933ebe7fc.jpg/v1/fill/w_378,h_378,al_c,q_80,usm_0.66_1.00_0.01/d78df6_914d12d64a57420c99737b9933ebe7fc.jpg"
+            alt=""
+          />
+          <img
+            src="https://static.wixstatic.com/media/d78df6_afe9335eb67640c988d648f7671308e5.jpg/v1/fill/w_378,h_378,al_c,q_80,usm_0.66_1.00_0.01/d78df6_afe9335eb67640c988d648f7671308e5.jpg"
+            alt=""
+          />
+          <img
+            src="https://static.wixstatic.com/media/d78df6_24dc2b5011604bc7ada79cbd953a7c5a.jpg/v1/fill/w_378,h_378,al_c,q_80,usm_0.66_1.00_0.01/d78df6_24dc2b5011604bc7ada79cbd953a7c5a.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="footer">
+        <span>© 2023 Олег Марков. Сайт создан на Wix.com.</span>
+        facebook-square Flickr Black Square Twitter Square Pinterest Black
+        Square
+        image.png
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
